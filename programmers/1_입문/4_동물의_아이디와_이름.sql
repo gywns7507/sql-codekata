476f333d75bce32186f2aa56c11038c63a77848f
gywns7507 <90439116+gywns7507@users.noreply.github.com>
2026-01-22 09:36:58 +0900

    [프로그래머스] 입문 - 동물의 아이디와 이름 (#4)

---
+++ programmers/1_입문/4_동물의_아이디와_이름.sql
@@ -0,0 +1,12 @@
+-- 동물의 아이디와 이름
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59403
+-- 작성자: 김효준
+-- 작성일: 2026. 01. 22. 09:36:55
+
+-- 코드를 입력하세요
+SELECT
+    ANIMAL_ID,
+    NAME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
