2cdcb063210989d0d94996b4fc9076929d24a0fe
gywns7507 <90439116+gywns7507@users.noreply.github.com>
2026-06-16 16:20:20 +0900

    [프로그래머스] 입문 - 모든 레코드 조회하기 (#47)

---
+++ programmers/1_입문/47_모든_레코드_조회하기.sql
@@ -0,0 +1,10 @@
+-- 모든 레코드 조회하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59034
+-- 작성자: 김효준
+-- 작성일: 2026. 06. 16. 16:20:20
+
+-- 코드를 입력하세요
+SELECT *
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
