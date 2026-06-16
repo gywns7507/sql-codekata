6c24a1af50d569bc36f09e1e6bdabc1520b8dd2f
gywns7507 <90439116+gywns7507@users.noreply.github.com>
2026-06-16 15:37:29 +0900

    [프로그래머스] 입문 - 가장 비싼 상품 구하기 (#14)

---
+++ programmers/1_입문/14_가장_비싼_상품_구하기.sql
@@ -0,0 +1,9 @@
+-- 가장 비싼 상품 구하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131697
+-- 작성자: 김효준
+-- 작성일: 2026. 06. 16. 15:37:28
+
+-- 코드를 입력하세요
+SELECT MAX(PRICE) AS MAX_PRICE
+FROM PRODUCT
